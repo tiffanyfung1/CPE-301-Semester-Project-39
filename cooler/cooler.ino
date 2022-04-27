55cb52e38a0d1061cba5729b077c8a963a55564f
brycemillis <70339482+brycemillis@users.noreply.github.com>
2022-04-27 10:37:55 -0700

    Update cooler.ino

--- cooler/cooler.ino
+++ cooler/cooler.ino
@@ -4,10 +4,16 @@
 // Parker True, Tiffany Fung, Bryce Millis
 
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <dht.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //library initialized w/ the numbers of interface pins
 
 // Main program constants and variables
+#define DHT_ANALOGPIN A0
+#define DHTTYPE DHT11
 #define LCD_REFRESH 60
 #define TEMPERATURE_LIMIT ##
+dht DHT;
 int state; // 0=disabled, 1=idle, 2=running, 3=error
 
 // I/O Macros
@@ -18,6 +24,7 @@ void setup(){
   Serial.begin(9600);
   setState(0); // disabled
   resetTimer(LCD_REFRESH);
+  lcd.begin(16, 2); 
 }
 
 void loop(){
@@ -75,16 +82,39 @@ void setFan(int fanOn){
     // setState(1); // idle
 */
 
-/* Timer Interrupt:
-  if(state != 0){
+// Timer Interrupt:
+ // if(state != 0){ 
     // Display humidity and temperature on LCD
+    float humidity = dht.readHumidity(); // read humidity
+    float tempF = dht.readTemperature(); // read temp in Farenheit
+    lcd.setCursor(0, 1);
+if (isnan(humidity) || isnan(tempF)) { //check for failed readings
+  lcd.println("DHT sensor reading(s) failed");
+} else {
+  lcd.println("Humidity: ");
+  lcd.print(humidity);
+  lcd.print("%");
+  lcd.println("Temperature: ");
+  lcd.print(tempF);
+  lcd.print("F");
   }
   // resetTimer(LCD_REFRESH);
-*/
+
 
 // NOTE: Can also be done using ADC
-/* Comparator Interrupt: // waterLevel < threshold
-  // Write error message to LCD
+// Comparator Interrupt: // waterLevel < threshold
+int threshVal = 0; // value holder
+int sensPin = A3; //  sensor pin 
+
+lcd.setCursor(0, 1);
+
+threshVal = analogRead(senspin); // read from analog pin, store in threshVal 
+
+if (thresVal<=300){
+  lcd.println("Water level is too low"); // LCD error message
+}
+delay(1000);
+  
+  
   // setFan(0);   // fan off
   // setState(3); // error
-*/
