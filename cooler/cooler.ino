7b618c4b72c9179a573733072f46c2144aa6be7f
brycemillis <70339482+brycemillis@users.noreply.github.com>
2022-05-01 15:17:50 -0700

    Update cooler.ino

--- cooler/cooler.ino
+++ cooler/cooler.ino
@@ -80,12 +80,36 @@ unsigned long nextLCDRefresh;
 volatile int state; // 0=disabled, 1=idle, 2=running, 3=error
 const char* stateNames[] = {"DISABLED", "IDLE", "RUNNING", "ERROR");
 
+int cwSwitch = 2; //clockwise
+int ccwSwitch = 3; //counter clock wise 
+int Pin1 = 10; //IN1 
+int Pin2 = 11; //IN2
+int Pin3 = 12; //IN3
+int Pin4 = 13; //IN4
+
+int pole1[] ={0,0,0,0, 0,1,1,1, 0}; //poles each with 8 step vals
+int pole2[] ={0,0,0,1, 1,1,0,0, 0};
+int pole3[] ={0,1,1,1, 0,0,0,0, 0};
+int pole4[] ={1,1,0,0, 0,0,0,1, 0};
+
+int stepperPole = 0;
+int direction = 0;			   
+			    
+
 void setup(){  
   // Interrupt variables
   PCICR |= 1 << PCIE1; // set to generate interrupts
   PCMSK1 |= 1 << PCINT10; // enable on pin 5
   DDRJ &= 0 << DD1; // set to input with pullup enabled
   PORTJ |= 1 << DD1;
+	
+ pinMode(Pin1, OUTPUT); //define pin for ULN2003 in1 
+ pinMode(Pin2, OUTPUT); //define pin for ULN2003 in2   
+ pinMode(Pin3, OUTPUT); //define pin for ULN2003 in3   
+ pinMode(Pin4, OUTPUT); //define pin for ULN2003 in4    
+
+ pinMode(cwSwitch,INPUT_PULLUP);
+ pinMode(ccwSwitch,INPUT_PULLUP);
   
   DDRE |= 0x3A; // 0b0011_1010, LED outputs
   nextLCDRefresh = 0;
@@ -99,6 +123,32 @@ void loop(){
   // If state != 0 & fan adjust input
     // Adjust fan position
     // Serial port: report vent position change
+	
+if(digitalRead(ccwSwitch) == LOW) //loop for counterclockwise/clockwise buttons
+  {
+    direction =1;
+  }else if(digitalRead(cwSwitch) == LOW)
+  {
+   direction  = 2;  
+  }else
+  {
+    direction =3; 
+  }
+ if(direction ==1){ 
+   stepperPole++; 
+    driveStepper(poleStep);    
+ }else if(direction ==2){ 
+   stepperPole--; 
+    driveStepper(poleStep);    
+ }else{
+  driveStepper(8);   
+ }
+ if(stepperPole>7){ 
+   stepperPole=0; 
+ } 
+ if(stepperPole<0){ 
+   stepperPole=7; 
+ } 
   
   // Temperature > threshold: turn on fan
   if(state = 1 & dht.readTemperature() > TEMPERATURE_LIMIT){
@@ -176,6 +226,9 @@ ISR(PCINT1_vect){
 
 // NOTE: Can also be done using ADC
 // Comparator Interrupt: // waterLevel < threshold
+void waterLevelTest()
+{
+	
 int threshVal = 0; // value holder
 int sensPin = A8; //  sensor pin 
 
@@ -187,67 +240,16 @@ if (thresVal<=300){
   lcd.println("Water level is too low"); // LCD error message
 }
 //resetTimer(LCD_REFRESH);
-  
-  
-  // setFan(0);   // fan off
-  // setState(3); // error
-int cwSwitch = 2; //clockwise
-int ccwSwitch = 3; //counter clock wise 
-int Pin1 = 10; //IN1 
-int Pin2 = 11; //IN2
-int Pin3 = 12; //IN3
-int Pin4 = 13; //IN4
+}  
+ 
 
-int pole1[] ={0,0,0,0, 0,1,1,1, 0}; //poles each with 8 step vals
-int pole2[] ={0,0,0,1, 1,1,0,0, 0};
-int pole3[] ={0,1,1,1, 0,0,0,0, 0};
-int pole4[] ={1,1,0,0, 0,0,0,1, 0};
 
-int stepperPole = 0;
-int direction = 0;
 
-void setup()
-{ //functions need to be changed to manual port registration 
- pinMode(Pin1, OUTPUT); //define pin for ULN2003 in1 
- pinMode(Pin2, OUTPUT); //define pin for ULN2003 in2   
- pinMode(Pin3, OUTPUT); //define pin for ULN2003 in3   
- pinMode(Pin4, OUTPUT); //define pin for ULN2003 in4    
-
- pinMode(cwSwitch,INPUT_PULLUP);
- pinMode(ccwSwitch,INPUT_PULLUP);
-}
-
-void loop()
-{
-  if(digitalRead(ccwSwitch) == LOW) 
-  {
-    direction =1;
-  }else if(digitalRead(cwSwitch) == LOW)
-  {
-   direction  = 2;  
-  }else
-  {
-    direction =3; 
-  }
- if(direction ==1){ 
-   stepperPole++; 
-    driveStepper(poleStep);    
- }else if(direction ==2){ 
-   stepperPole--; 
-    driveStepper(poleStep);    
- }else{
-  driveStepper(8);   
- }
- if(stepperPole>7){ 
-   stepperPole=0; 
- } 
- if(stepperPole<0){ 
-   stepperPole=7; 
- } 
 //add reset timer (alternative for delay function) here
   
   void stepMotor(int step)
   {
+
      digitalWrite(Pin1, pole1[step]);  
      digitalWrite(Pin2, pole2[step]); 
      digitalWrite(Pin3, pole3[step]); 
